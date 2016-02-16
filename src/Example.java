147c166fee96483dea4dd9ca2652fb7d5957d68f
NAE <nileproject@hotmail.com>
2016-02-15 20:41:20 -0600

    Fixing the build

--- src/Example.java
+++ src/Example.java
@@ -8,7 +8,7 @@ public class Example {
 	 * Adds two integers
 	 */
 	public int add(int a, int b) {
-		return a + b + b;
+		return a + b;
 	}
 	
 	/**
@@ -22,7 +22,7 @@ public class Example {
 	 * Multiplies two integers
 	 */
 	public int multiply(int a, int b) {
-		return a * b * b;
+		return a * b;
 	}
 	
 	/**
